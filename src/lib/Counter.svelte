b1b0684851f93a05a815d74452ec3a120c211a4d
phatt-23 <phat.trandai23@gmail.com>
2025-09-18 19:20:28 +0200

    init

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  let count: number = $state(0)
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button onclick={() => {
+  count += 1
+}}>
+  count is {count}
+</button>
